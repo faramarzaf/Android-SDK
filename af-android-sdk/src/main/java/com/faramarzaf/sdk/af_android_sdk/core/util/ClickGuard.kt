d14700436b8f9a30f4e74304334670e297fbfa57
FaramarzAf <faramarzafzali@gmail.com>
2020-07-08 16:12:10 +0430

    clickGuard added

---
+++ af-android-sdk/src/main/java/com/faramarzaf/sdk/af_android_sdk/core/util/ClickGuard.kt
@@ -0,0 +1,45 @@
+package com.faramarzaf.sdk.af_android_sdk.core.util
+
+import android.os.SystemClock
+import android.view.View
+import com.faramarzaf.sdk.af_android_sdk.core.interfaces.DoGuardTask
+/**
+ * @author Faramarz Afzali
+ * 22/June/2020
+ * <p>
+ * This class is supposed to prevent fast and rapid End-user's clicks on views
+ * <p>
+ */
+
+class ClickGuard {
+
+
+    companion object {
+        private var mLastClickTime: Long = 0
+
+        fun guardView(view: View, delay: Long, callback: DoGuardTask) {
+            view.setOnClickListener { v ->
+                if (SystemClock.elapsedRealtime() - mLastClickTime < delay) {
+                    return@setOnClickListener
+                }
+                mLastClickTime = SystemClock.elapsedRealtime()
+                callback.onGuard(v)
+            }
+        }
+
+
+        fun guardView(view: List<View>, delay: Long, doGuardTask: DoGuardTask?) {
+            for (i in view.indices) {
+                view[i].setOnClickListener(View.OnClickListener { v ->
+                    if (SystemClock.elapsedRealtime() - mLastClickTime < delay) {
+                        return@OnClickListener
+                    }
+                    mLastClickTime = SystemClock.elapsedRealtime()
+                    doGuardTask?.onGuard(v)
+                })
+            }
+        }
+    }
+
+
+}
--- app/src/main/java/com/faramarzaf/sdk/android/MainActivity.kt
+++ app/src/main/java/com/faramarzaf/sdk/android/MainActivity.kt
@@ -1,11 +1,7 @@
 package com.faramarzaf.sdk.android
 
-import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.faramarzaf.sdk.af_android_sdk.core.interfaces.DialogCallback
-import com.faramarzaf.sdk.af_android_sdk.core.ui.dialog.SimpleDialog
-import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,35 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btnShow.setOnClickListener {
-            showAlertDialog()
-        }
-    }
-
-
-    fun showAlertDialog() {
-        SimpleDialog(this)
-            .cancelable(true)
-            .setNegativeButton("Text_Negative")
-            .setPositiveButton("Text_Positive")
-            .setText("This is a title")
-            .showDialog()
-            .setTitleColor(Color.YELLOW)
-            .setTypeface("vazir_thin.ttf")
-            .setTextSize(28f, 12f)
-            .setImageDialogBackground(R.drawable.ic_launcher_foreground)
-            .setDialogBackground(R.drawable.shape_round_corner_dialog)
-            .setPositiveBackground(R.drawable.shape_1, Color.RED)
-            .setNegativeBackground(R.drawable.shape_2, Color.rgb(255, 255, 255))
-            .setCallBack(object : DialogCallback {
-                override fun onPositiveButtonClicked() {
-                    // do your work
-                }
-
-                override fun onNegativeButtonClicked() {
-                    // do your work
-                }
-            })
     }
 
 }
+
