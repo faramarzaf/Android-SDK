d83fa781542b0615906e6acb79dec2a25ff2ba75
FaramarzAf <faramarzafzali@gmail.com>
2020-07-20 13:18:27 +0430

    hideToolbar added

--- af-android-sdk/src/main/java/com/faramarzaf/sdk/af_android_sdk/core/helper/NetworkHelper.kt
+++ af-android-sdk/src/main/java/com/faramarzaf/sdk/af_android_sdk/core/helper/NetworkHelper.kt
@@ -12,9 +12,7 @@ class NetworkHelper {
 
         private fun isNetworkConnected(context: Context): Boolean {
             val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-            return Objects.requireNonNull(cm).activeNetworkInfo != null && Objects.requireNonNull<NetworkInfo>(
-                cm.activeNetworkInfo
-            ).isConnected
+            return Objects.requireNonNull(cm).activeNetworkInfo != null && Objects.requireNonNull<NetworkInfo>(cm.activeNetworkInfo).isConnected
         }
 
         fun checkNetwork(context: Context): Boolean {
--- af-android-sdk/src/main/java/com/faramarzaf/sdk/af_android_sdk/core/helper/ScreenHelper.kt
+++ af-android-sdk/src/main/java/com/faramarzaf/sdk/af_android_sdk/core/helper/ScreenHelper.kt
@@ -2,7 +2,12 @@ package com.faramarzaf.sdk.af_android_sdk.core.helper
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.Color
+import android.os.Build
 import android.util.DisplayMetrics
+import android.view.View
+import android.view.WindowManager
+import androidx.annotation.RequiresApi
 
 class ScreenHelper {
 
@@ -24,6 +29,14 @@ class ScreenHelper {
 
         fun getScreenHeight(context: Context): Int {
             return context.resources.displayMetrics.heightPixels
+        }
+
+        @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+        fun hideToolbar(activity: Activity) {
+            val window = activity.window
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = Color.TRANSPARENT
         }
     }
 }
