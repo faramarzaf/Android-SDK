ae535eac9291260482c032796cc51b1f0bebe12d
FaramarzAf <faramarzafzali@gmail.com>
2020-07-21 12:24:38 +0430

    public dialog added

---
+++ af-android-sdk/src/main/java/com/faramarzaf/sdk/af_android_sdk/core/ui/dialog/PublicDialog.kt
@@ -0,0 +1,30 @@
+package com.faramarzaf.sdk.af_android_sdk.core.ui.dialog
+
+import android.content.Context
+import androidx.appcompat.app.AlertDialog
+import com.faramarzaf.sdk.af_android_sdk.core.interfaces.DialogCallback
+
+class PublicDialog {
+
+
+    companion object {
+
+
+        fun yesNoDialog(context: Context, title: String, message: String, textYes: String, textNo: String, icon: Int, callback: DialogCallback) {
+            AlertDialog.Builder(context)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton(textYes) { dialog, which ->
+                    callback.onPositiveButtonClicked()
+                }
+                .setNegativeButton(textNo) { dialog, which ->
+                    callback.onNegativeButtonClicked()
+                }
+                .setIcon(icon)
+                .show()
+        }
+
+    }
+
+
+}
